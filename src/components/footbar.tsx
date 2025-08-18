ba3965d1748e1147e202a1d44c84753eed18f926
Allen <s96016641@gmail.com>
2025-08-18 10:55:13 +0800

    feat: integrate Divider component into Footbar and update layout

--- src/components/footbar.tsx
+++ src/components/footbar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@heroui/link";
+import { Divider } from "@heroui/divider";
 import React from "react";
 
 import { GithubIcon, HeartFilledIcon } from "@/components/icons.tsx";
@@ -12,7 +13,7 @@ const FooterLink = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="flex space-x-4">
+    <div className="space-x-4">
       <Link
         className="text-default-600 hover:text-primary"
         href={link}
@@ -28,20 +29,18 @@ export const Footbar = () => {
   return (
     <>
       <footer className="w-full flex items-center justify-center">
-        <div className="container mx-auto max-w-7xl px-12 md:px-0 pt-12">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="space-y-4">
-              <h3 className="text-lg font-bold md:text-left text-center">
-                校方連結
-              </h3>
-              <div className="flex space-x-4">
+        <div className="container mx-auto max-w-7xl px-12 pt-12">
+          <div className="sm:flex gap-8">
+            <div className="space-y-4 flex-1 text-left sm:text-right">
+              <h3 className="text-lg font-bold">校方連結</h3>
+              <div className="space-x-4">
                 <h4 className="text-default-600">還沒有整理啦</h4>
               </div>
             </div>
-            <div className="space-y-4">
-              <h3 className="text-lg font-bold md:text-left text-center">
-                關於專案
-              </h3>
+            <Divider className="h-auto hidden sm:flex" orientation="vertical" />
+            <Divider className="w-auto sm:hidden my-3" />
+            <div className="space-y-4 flex-1">
+              <h3 className="text-lg font-bold">關於專案</h3>
               <FooterLink link="https://github.com/allen0099/utaipei-course-web">
                 <GithubIcon className="mr-2" />
                 網頁原始碼
