df3d66107b902ce78412c4640eefe2828015ab08
Allen <s96016641@gmail.com>
2025-08-19 11:23:39 +0800

    style: improve icon component structure and formatting

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -9,8 +9,10 @@ export default function NotFoundPage() {
         <div className="text-center">
           <h1 className="text-4xl font-bold">404</h1>
           <p className="text-xl mt-2">Page Not Found</p>
-          <p className="text-default-500 mt-2">The page you are looking for does not exist.</p>
-          <Link href="/" className="mt-6">
+          <p className="text-default-500 mt-2">
+            The page you are looking for does not exist.
+          </p>
+          <Link className="mt-6" href="/">
             Go back to Home
           </Link>
         </div>
