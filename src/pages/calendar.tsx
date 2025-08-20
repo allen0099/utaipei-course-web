4542a0390211df306bf4eaea9a6167b6b59a4e58
Allen <s96016641@gmail.com>
2025-08-20 12:13:09 +0800

    feat(calendar): add calendar viewer and responsive page component
    - Introduced `CalendarViewer` component to display PDF documents.
    - Added `ResponsivePage` component for handling page resizing.
    - Integrated `useResizeObserver` from `@wojtekmaj/react-hooks` for responsive behavior.
    - Updated `CalendarPage` to utilize the new components and improved layout.

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,18 +1,77 @@
 import { Spinner } from "@heroui/spinner";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Button } from "@heroui/button";
+import { Document, Page, PageProps } from "react-pdf";
 import { pdfjs } from "react-pdf";
+import { useResizeObserver } from "@wojtekmaj/react-hooks";
 
 import DefaultLayout from "@/layouts/default";
 import { siteConfig } from "@/config/site.ts";
 import { calendarItem } from "@/interfaces/globals.ts";
 import { title } from "@/components/primitives.ts";
 
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+const ResponsivePage = (props: PageProps) => {
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const onResize = useCallback(() => {
+    if (containerRef) {
+      setContainerWidth(containerRef.clientWidth);
+    }
+  }, [containerRef]);
+
+  useResizeObserver(containerRef, {}, onResize);
+
+  return (
+    <div ref={setContainerRef}>
+      <Page {...props} width={containerWidth} />
+    </div>
+  );
+};
+
+export const CalendarViewer = ({ link }: { link: string }) => {
+  const [numPages, setNumPages] = useState<number>(1);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-5xl">
+        <Document
+          className="flex flex-wrap justify-center gap-4"
+          file={link}
+          loading={<Spinner />}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {Array.from(new Array(numPages), (_, index) => (
+            <div
+              key={`page_wrapper_${index + 1}`}
+              className="w-full xl:w-[48%]"
+            >
+              <ResponsivePage
+                key={`page_${index + 1}`}
+                className="dark:invert dark:hue-rotate-180 shadow-lg"
+                pageNumber={index + 1}
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+              />
+            </div>
+          ))}
+        </Document>
+      </div>
+    </div>
+  );
+};
 
 export const CalendarPage = () => {
   const [calendar, setCalendar] = useState<calendarItem[]>([]);
-  const [loading, setLoading] = useState(true);
 
   const lastItem = calendar[calendar.length - 1];
 
@@ -24,19 +83,20 @@ export const CalendarPage = () => {
           item.link = `${siteConfig.links.github.api}/calendar/${item.year}/${item.title}.pdf`;
         });
         setCalendar(data);
-        setLoading(false);
-      })
-      .catch(() => setLoading(false));
+      });
   }, []);
 
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <h1 className={title()}>{lastItem?.title || "校園行事曆"}</h1>
-        <Button color="primary" variant="ghost">
-          切換年分
-        </Button>
-        {loading ? <Spinner /> : <></>}
+        <div className="flex max-sm:flex-col max-lg:w-full items-center">
+          <h1 className={title()}>{lastItem?.title || "校園行事曆"}</h1>
+          <Button className="ml-8" color="primary" variant="ghost">
+            切換年分
+          </Button>
+        </div>
+
+        <CalendarViewer link={lastItem?.link} />
       </section>
     </DefaultLayout>
   );
