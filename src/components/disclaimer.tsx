36f510e6f97fc9fa99ee88bfc268d350de437384
秉虎 <s96016641@gmail.com>
2025-08-30 01:07:42 +0800

    feat(disclaimer): enhance dark mode support and adjust modal size

--- src/components/disclaimer.tsx
+++ src/components/disclaimer.tsx
@@ -49,10 +49,8 @@ export const DisclaimerModal = () => {
       backdrop="blur"
       classNames={{
         // body: "py-6",
-        // backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
-        // base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+        base: "dark:bg-[#19172c]",
         header: "text-danger",
-        // footer: "border-t-[1px] border-[#292f46]",
         closeButton: "hover:bg-white/5 active:bg-white/10",
       }}
       hideCloseButton={true}
@@ -60,7 +58,7 @@ export const DisclaimerModal = () => {
       isKeyboardDismissDisabled={true}
       isOpen={open}
       radius="lg"
-      size="2xl"
+      size="3xl"
       onOpenChange={setOpen}
     >
       <ModalContent>
@@ -68,8 +66,10 @@ export const DisclaimerModal = () => {
           <p>免責聲明</p>
         </ModalHeader>
         <ModalBody>
-          <p className="mb-4 text-gray-700">
-            本網站為<span className="text-red-600">非官方</span>課程查詢工具，
+          <p className="mb-4 text-gray-700 dark:text-gray-300">
+            本網站為
+            <span className="text-red-600">非官方</span>
+            課程查詢工具，
             所有資料均由學校公開選課系統取得，僅供個人與同學參考之用。
             <br />
             資料可能因系統更新或網路狀況產生延遲或錯誤，
@@ -78,7 +78,7 @@ export const DisclaimerModal = () => {
             使用本網站即表示您已閱讀並同意以下內容：
           </p>
 
-          <ul className="mb-4 text-gray-700 list-disc list-inside space-y-1">
+          <ul className="mb-4 text-gray-700 dark:text-gray-300 list-disc list-inside space-y-1">
             <li>本網站與學校無任何隸屬或授權關係，不代表學校立場。</li>
             <li>
               課程資訊僅供參考，
@@ -89,13 +89,13 @@ export const DisclaimerModal = () => {
             <li>本站不蒐集、不處理、不揭露任何學生個人資料。</li>
           </ul>
 
-          <p className="mb-4 text-gray-700">
+          <p className="mb-4 text-gray-700 dark:text-gray-300">
             為改善使用體驗，本網站可能會在您的裝置上存取
             Cookie。若您不願接受，您可於瀏覽器設定中調整隱私權等級以阻擋
             Cookie，但可能導致部分功能無法正常運作。
           </p>
 
-          <div className="text-xs text-gray-500 mt-2">
+          <div className="text-xs text-gray-500 dark:text-gray-100 mt-2">
             聯絡信箱：allen0099[at]sudo.host
             <br />
             最後更新：2025-08-24
